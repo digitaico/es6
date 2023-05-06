7e12ba702824e463341a607133d137581dec06cd
Jorge Eduardo Ardila <jea.data@gmail.com>
2023-05-05 19:29:27 -0500

    more tests

---
+++ reverse-matrices.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const m = [
+  [112, 42, 83, 119],
+  [56, 125, 56, 49],
+  [15, 78, 101, 43],
+  [62, 98, 114, 108],
+];
+
+// identificar column mas alta = sum mayor
+// reversarla para que mayor valor quede en primera fila
+// identificar fila con mayor valor = suma
+// reversarla para que mayor valor quede en esquina izquierda
+//
