ad65dc56e83b729c41b0031d8657ac58f38461e6
Jorge Eduardo Ardila <jea.data@gmail.com>
2023-05-26 10:49:46 -0500

    finished chall

--- between-two-sets.js
+++ between-two-sets.js
@@ -1,44 +1,23 @@
 'use strict';
 // uses LCM and GCD.
 
-const arr1 = [3,9,6];
-const arr2 = [36,72];
-// R : 2
+const arr1 = [2];
+const arr2 = [20, 30, 12];
+// R : 1
 
-
-const getMin = (arr) => {
-  return arr.reduce((x, y) => Math.min(x, y), +Infinity);
-}
-
-const getMax = (arr) => {
-  return arr.reduce((a, b) => Math.max(a, b), -Infinity);
-}
-
-const getGcd = (min, max) => {
-  return !max ? min: getGcd(max, min % max);
-}
-
-const getLcm = (arr) => {
-  const min = getMin(arr);
-  const max = getMax(arr);
-  if (min == 0 || max == 0) {
-    return 0;
-  } else {
-    return Math.abs((min * max)) / getGcd(min,max);
-  }
-}
+const getGcd = (a, b) => b == 0 ? a : getGcd(b, a % b);
+const getLcm = (a, b) => a / getGcd(a, b) * b;
+const getLcmAll = (arr) => arr.reduce(getLcm, 1);
+const getGcdAll = (arr) => arr.reduce(getGcd, 0);
 
 const betweenTwoSets = (a, b) => {
   let result = 0;
   let multiple = 0;
-  const lcm = getLcm(a);
-  const min = getMin(b);
-  const max = getMax(b);
-  const gcd = getGcd(min, max);
+  const lcm = getLcmAll(a);
+  const gcd = getGcdAll(b);
 
   while (multiple <= gcd ) {
     multiple += lcm;
-    console.log(gcd)
     if (gcd % multiple == 0) {
       result++;
     }
@@ -46,4 +25,3 @@ const betweenTwoSets = (a, b) => {
   return result;
 }
 console.log(betweenTwoSets(arr1, arr2));
-//betweenTwoSets(arr1, arr2);
