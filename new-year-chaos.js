2c5d8f51b3757a12f03d9d8d92724685ce3b661c
Jorge Eduardo Ardila <jea.data@gmail.com>
2023-06-15 08:14:15 -0500

    test

--- new-year-chaos.js
+++ new-year-chaos.js
@@ -1,22 +1,24 @@
-var q = [2, 5, 1, 3, 4];
+var q = [2, 1, 5, 3, 4];
+
 var isOrdered = function (arr) {
-    arr.every(function (x, i) {
+    return arr.every(function (x, i) {
         return i === 0 || x >= q[i - 1];
     });
 };
-console.log(isOrdered(q));
-/*
-const minimumBribes(q: number[]): void => {
-  let minBribes = 0;
-  const nChanges = new Array(q.length).fill(0);
+//console.log(isOrdered(q));
 
-  while(!isOrdered(q)) {
-    for (let i = 0; i < q.length - 1; i++) {
-      if(q[i] > q[i + 1]) {
-        nChanges[q[i] - 1]++
-
-      }
-    }
-  }
+const bribes = (q) => {
+    const que = [1,2,3,4,5,6,7,8];
+    const res = [];
+    // comparar dos colas: final con inicial
+    // comparar posiciones: findIndex(el);
+    // estimar diferencia entre posicion inicial y final.
+    q.forEach((el, index) => {
+        if (el != que[index]) {
+            res.push(index);
+        }
+    })
+    return res;
 }
-*/
+
+console.log(bribes(q));
