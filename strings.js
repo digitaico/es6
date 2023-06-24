86d0e041b7f9e5daa9ef6d633346f88166aac14b
Jorge Eduardo Ardila <jea.data@gmail.com>
2023-06-24 18:26:44 -0500

    added cases

--- strings.js
+++ strings.js
@@ -75,6 +75,7 @@ const countVowelsConsonants = (str) => {
   str = str.toLowerCase();
   const vowelsRegex = /[aeiou]/g;
   const consonantsRegex = /[b-df-hj-np-tv-xz]/g;
+
   const resVowels = [...str.matchAll(vowelsRegex)];
   const resConsonants = [...str.matchAll(consonantsRegex)];
   return [resVowels.length, resConsonants.length];
@@ -91,37 +92,72 @@ const countChar = (char, str) => {
 
 // 9. find all permutations of a string.
 const str9 = 'dig';
+
 const findStringPermutations = (str) => {
   if(str.length < 2) return str;
+  const permutations = [];
 
-  let permutations = [];
   for (let i = 0; i < str.length; i++) {
     const char = str[i];
-
-    if(str.indexOf(char) != i)
+    // avoid duplicates
+    if (str.indexOf(char) != i) 
       continue;
 
-    const remainingString = str.slice(0, i) + str.slice(i + 1, str.length);
-
-    for (let sp of findStringPermutations(remainingString))
-      permutations.push(char + sp);
+    const remStr = str.slice(0, i) + str.slice(i + 1, str.length);
+    for (const permut of findStringPermutations(remStr)) {
+      permutations.push(char + permut);
+    }
   }
   return permutations;
 }
 //console.log(findStringPermutations(str9));
 
+
 // 10. reverse words in given sentence without using library method.
-// see 4
+// 'Maria is the queen of Harlem' => 'Harlem of queen the is Maria'
+const str10 = 'Jorge Eduardo Ardila Muñoz';
+
+const reverseSentence = (str) => {
+  return str.split(' ' ).reverse().join(' ');
+}
+//console.log(reverseSentence(str10));
 
 
-// 11. how to check if two strings are a rotation of each other.
+// 11. Check if two strings are a rotation of each other.
 const str11 = 'Jorge';
 const str110 = 'eJorg';
 
-const checkStringRotation = (str1, str2) => {
-  return [...str11]
+const findIndex = (str1, str2) => {
+  return [...str2].reduce((acc, val, index) => acc.concat(val === [...str1][0] ? index : []), []);
 }
-console.log(checkStringRotation(str11, str110))
+
+const checkStrings = (s1, s2, index, length) => {
+  for (let i = 0; i < length; i++) {
+    if (s1[i] != s2[(index + i) % length])
+      return false;
+  }
+  return true;
+}
+
+const checkStringRotation = (str1, str2) => {
+  if (str1.length != str2.length) {
+    return false;
+  }
+  // find index of str1 first char on str2.
+  let isRotated = false;
+  const indexes = findIndex(str1, str2);
+  const l = str1.length;
+
+  for (let idx of indexes ) {
+    isRotated = checkStrings(str1, str2, idx, l );
+    if (isRotated)
+      break;
+  }
+
+  return isRotated;
+}
+//console.log(checkStringRotation(str11, str110))
+
 
 // 12. check if a string is a palindrome.
 const str12 = 'tattarrattat';
